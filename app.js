838f7e6d1e7ebc62281e6f84f5340bc861dd4e4e
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-07-28 11:22:29 +1200

    Started Script, added results page, declared variables

--- app.js
+++
@@ -1,6 +0,0 @@
-const grainPerDay = 18.1437;
-const litrePerDay = 4163.953;
-const forestPerDay = 2.78709;
-const animalsPerDay = 1;
-
-
---
+++ convert.js
@@ -0,0 +1,32 @@
+//Declaring the savings per day
+const grainPerDay = 18.1437;
+const litrePerDay = 4163.953;
+const forestPerDay = 2.78709;
+const animalsPerDay = 1;
+
+//declaring other variables from inputs
+var days = document.getElementById("days");
+var months = document.getElementById("months");
+var years = document.getElementById("years");
+
+//Calculating the total days
+function calculateDays() {
+    var totalDays = days.value * 1 + months.value * 30.436875 + years.value * 365.2425;
+    return totalDays;
+}
+
+
+//Calculating the savings per day
+function calculateSavings() {
+    var grainSavings = (Math.floor(grainPerDay * totalDays));
+    document.getElementById("grainSavings").innerHTML;
+    var litreSavings = (Math.floor(litrePerDay * totalDays));
+    document.getElementById("litreSavings").innerHTML;
+    var forestSavings = (Math.floor(forestPerDay * totalDays));
+    document.getElementById("forestSavings").innerHTML;
+    var animalSavings = (Math.floor(animalsPerDay * totalDays));
+    document.getElementById("animalSavings").innerHTML;
+    return grainSavings, litreSavings, forestSavings, animalSavings;
+}
+
+
