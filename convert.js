e4b529d6ee41d96462e9b5fc36c3df085ecee427
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-07-28 13:37:29 +1200

    Played around more

--- convert.js
+++ convert.js
@@ -4,31 +4,19 @@ const litrePerDay = 4163.953;
 const forestPerDay = 2.78709;
 const animalsPerDay = 1;
 
-//declaring other variables from inputs
-var days = document.getElementById("days");
-var months = document.getElementById("months");
-var years = document.getElementById("years");
-var totalDays = calculateDays();
+//Converting the inputs to numbers
+function calculateDays() {
+    document.getElementById("days").value = days;
+    document.getElementById("months").value = months;
+    document.getElementById("years").value = years;
+}
+
+//creating a string for the days
+
 
 //Calculating the total days
-function calculateDays() {
-    var totalDays = days.value * 1 + months.value * 30.436875 + years.value * 365.2425;
-    return totalDays;
-}
+const totalDays = Math.floor(days.value * 1 + months.value * 30.436875 + years.value * 365.2425);
 
-
-//Calculating the savings per day
-function calculateSavings() {
-    var grainSavings = (Math.floor(grainPerDay * totalDays));
-    document.getElementById("grainSavings").innerHTML = grainSavings;
-    var litreSavings = (Math.floor(litrePerDay * totalDays));
-    document.getElementById("litreSavings").innerHTML;
-    var forestSavings = (Math.floor(forestPerDay * totalDays));
-    document.getElementById("forestSavings").innerHTML;
-    var animalSavings = (Math.floor(animalsPerDay * totalDays));
-    document.getElementById("animalSavings").innerHTML;
-    return grainSavings, litreSavings, forestSavings, animalSavings;
-}
 
 //Savings variables
 const grainSavings = (Math.floor(grainPerDay * totalDays));
@@ -37,4 +25,13 @@ const forestSavings = (Math.floor(forestPerDay * totalDays));
 const animalSavings = (Math.floor(animalsPerDay * totalDays));
 
 
+//creating functions
 
+function getTimeString() {
+    var timeString = days.value + " days, " + months.value + " months, " + years.value + " years";
+    document.getElementById("timeString").value = timeString;
+}
+
+console.log(timeString);
+
+fuction
