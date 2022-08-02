b53fa8dfc0dee9d5d438acbab196cb6555ac245b
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-08-02 14:28:39 +1200

    updates

--- convert.js
+++ convert.js
@@ -1,8 +1,36 @@
+//Refernce the form
+const myForm = document.getElementById('inputForm');
+
+//Initialise FormData constructor with the form
+const formData = new FormData(myForm);
+
+//Using formData.entries and Object.fromEntries to convert the formData to an object
+const formObject = Object.fromEntries(formData.entries());
+
+//Testing
+console.log(formObject);
+
+//Init Days
+const days = new FormData(myForm);
+
+const userDays = Object.fromEntries(formData.entries(days));
+
+console.log(userDays)
+
+//getting days from the form
+const inputDays = document.getElementById('days');
+const daysVal = new Object(inputDays.value);
+
+console.log(daysVal)
+
+/* NOT SURE IF THIS WORKS
+(() => {
 //Declaring the savings per day
 const grainPerDay = 18.1437;
 const litrePerDay = 4163.953;
 const forestPerDay = 2.78709;
 const animalsPerDay = 1;
+})();
 
 //Converting the inputs to numbers
 function getTotalDays() {
@@ -13,7 +41,6 @@ function getTotalDays() {
 }
 
 //show savings
-
 function showSavings() {
     const totalDays = getTotalDays();
     const grainSavings = grainPerDay * totalDays;
@@ -25,3 +52,5 @@ function showSavings() {
     document.getElementById("forest").innerHTML = forestSavings
     document.getElementById("animals").innerHTML = animalSavings
 }
+
+*/
