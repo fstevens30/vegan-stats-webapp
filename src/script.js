d84e79fbf221622adf0615f5bade497809a1696a
Flynn <flynn-stevens@live.nmit.ac.nz>
2022-08-16 14:27:04 +1200

    Refactor

--- src/script.js
+++ src/script.js
@@ -1,23 +1,20 @@
-//Setting const for the savings per day in each category
-const grainPerDay = 18.1437;
-const litrePerDay = 4163.953;
-const forestPerDay = 2.78709;
-const animalsPerDay = 1;
+document.getElementById('submit').addEventListener('click', function() {
+    const days = document.getElementById('vegan-days').value;
+    const months = document.getElementById('vegan-months').value;
+    const years = document.getElementById('vegan-years').value;
+    const totalDays = Math.floor((days) + (months * 30.42) + (years * 365.25));
+    console.log(totalDays);
 
+    const grainPerDay = 18.1437;
+    const litrePerDay = 4163.953;
+    const forestPerDay = 2.78709;
+    const animalsPerDay = 1;
 
-calculateSavings() {
-    //Setting the variables to the number of days/months/years inputted by the user in the form
-    let days = document.getElementById("days").value; //days
-    let months = document.getElementById("months").value; //months
-    let years = document.getElementById("years").value; //years
+    const grain = Math.floor(grainPerDay * totalDays);
+    const litre = Math.floor(litrePerDay * totalDays);
+    const forest = Math.floor(forestPerDay * totalDays);
+    const animals = Math.floor(animalsPerDay * totalDays);
 
-    //Combining the days, months and years into one variable
-    let totalDays = ((days) + (months * 30.417) + (years * 365.25));
-
-    //Calculating the total savings per day in each category
-    let grain = grainPerDay * totalDays;
-    let litre = litrePerDay * totalDays;
-    let forest = forestPerDay * totalDays;
-    let animals = animalsPerDay * totalDays;
-
-};
+    let message = `You have saved ${grain}kg of grain, ${litre}litre of water, ${forest}m2 of forest and the lives of${animals} animals.`;
+    document.getElementById('result').innerHTML = message;
+});
