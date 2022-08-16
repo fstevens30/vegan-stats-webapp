05cf2e1c647d97df237540b5cb63861c786c4d7f
Flynn <flynn-stevens@live.nmit.ac.nz>
2022-08-16 14:41:13 +1200

    Basic completetion

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ document.getElementById('submit').addEventListener('click', function() {
     const days = document.getElementById('vegan-days').value;
     const months = document.getElementById('vegan-months').value;
     const years = document.getElementById('vegan-years').value;
-    const totalDays = Math.floor((days) + (months * 30.42) + (years * 365.25));
+    const totalDays = Math.floor(days) + Math.floor(months) * 30.412 + Math.floor(years) * 365.25;
     console.log(totalDays);
 
     const grainPerDay = 18.1437;
@@ -10,11 +10,11 @@ document.getElementById('submit').addEventListener('click', function() {
     const forestPerDay = 2.78709;
     const animalsPerDay = 1;
 
-    const grain = Math.floor(grainPerDay * totalDays);
-    const litre = Math.floor(litrePerDay * totalDays);
-    const forest = Math.floor(forestPerDay * totalDays);
-    const animals = Math.floor(animalsPerDay * totalDays);
+    const grain = Math.floor(grainPerDay * totalDays).toLocaleString();
+    const litre = Math.floor(litrePerDay * totalDays).toLocaleString();
+    const forest = Math.floor(forestPerDay * totalDays).toLocaleString();
+    const animals = Math.floor(animalsPerDay * totalDays).toLocaleString();
 
-    let message = `You have saved ${grain}kg of grain, ${litre}litre of water, ${forest}m2 of forest and the lives of${animals} animals.`;
-    document.getElementById('result').innerHTML = message;
+    let message = `You have saved ${grain} kg of grain, ${litre} litres of water, ${forest} m2 of forest and the lives of ${animals} animals.`;
+    document.getElementById('results').innerHTML = message;
 });
