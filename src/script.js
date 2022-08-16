207cdf8eeae986b36d5de0f92d1ff2af7afcb8c1
Flynn <flynn-stevens@live.nmit.ac.nz>
2022-08-16 14:55:16 +1200

    Edited strings for results

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ document.getElementById('submit').addEventListener('click', function() {
     const months = document.getElementById('vegan-months').value;
     const years = document.getElementById('vegan-years').value;
     const totalDays = Math.floor(days) + Math.floor(months) * 30.412 + Math.floor(years) * 365.25;
-    console.log(totalDays);
+    document.getElementById('vegan-total-days').innerHTML = Math.floor(totalDays);
 
     const grainPerDay = 18.1437;
     const litrePerDay = 4163.953;
@@ -15,6 +15,8 @@ document.getElementById('submit').addEventListener('click', function() {
     const forest = Math.floor(forestPerDay * totalDays).toLocaleString();
     const animals = Math.floor(animalsPerDay * totalDays).toLocaleString();
 
-    let message = `You have saved ${grain} kg of grain, ${litre} litres of water, ${forest} m2 of forest and the lives of ${animals} animals.`;
-    document.getElementById('results').innerHTML = message;
+    document.getElementById('grain').innerHTML = grain;
+    document.getElementById('litre').innerHTML = litre;
+    document.getElementById('forest').innerHTML = forest;
+    document.getElementById('animals').innerHTML = animals;
 });
